02a96375bc999c7649f5f62a3ced8843c4b9c152
Xiaolong Huang <xiaoloonghuang@gmail.com>
2025-12-26 18:42:08 +0800

    remote build use ccache to accelerate

--- scripts/remote_build.sh
+++ scripts/remote_build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+TASK_FILE="tasks/build_jetson_cross.yaml"
+if [[ $# -gt 0 ]]; then
+    TASK_FILE=$1
+fi
+
 PROGRESS_ARGS=""
 # PROGRESS_ARGS="--info=progress2 --progress"
-
 
 echo Synchronizing source code
 rsync -a ${PROGRESS_ARGS} ./ g:/home/xiaolonghuang/mllm/ --exclude={"data","perf.*",".git","build"}
 
-ssh g 'source ~/.zshrc; cd /home/xiaolonghuang/mllm/ && python3 task.py tasks/build_jetson_cross.yaml' | \
+ssh g "source ~/.zshrc; cd /home/xiaolonghuang/mllm/ && CCACHE_SLOPPINESS=time_macros python3 task.py ${TASK_FILE}" | \
     sed -e 's/\/home\/xiaolonghuang\/mllm/\/home\/xiaolong\/mllm/g' \
         -e 's/--sysroot[^ ]* //' \
         -e 's/\/home\/xiaolonghuang\/l4t-gcc\/bin\/aarch64-buildroot-linux-gnu-g++/\/usr\/bin\/g++/'
