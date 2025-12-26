cc703e27d2198b107b94a800b7fc6e10ae8f9309
Xiaolong Huang <xiaoloonghuang@gmail.com>
2025-12-26 17:17:58 +0800

    better remote build script

--- scripts/remote_build.sh
+++ scripts/remote_build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo Synchronizing source code
-rsync -a --info=progress2 --progress ./ g:/home/xiaolonghuang/mllm/ --exclude={"data","perf.*",".git","build"}
+PROGRESS_ARGS=""
+# PROGRESS_ARGS="--info=progress2 --progress"
 
-ssh g 'source ~/.zshrc; cd /home/xiaolonghuang/mllm/ && python3 task.py tasks/build_jetson_cross.yaml'
+
+echo Synchronizing source code
+rsync -a ${PROGRESS_ARGS} ./ g:/home/xiaolonghuang/mllm/ --exclude={"data","perf.*",".git","build"}
+
+ssh g 'source ~/.zshrc; cd /home/xiaolonghuang/mllm/ && python3 task.py tasks/build_jetson_cross.yaml' | \
+    sed -e 's/\/home\/xiaolonghuang\/mllm/\/home\/xiaolong\/mllm/g' \
+        -e 's/--sysroot[^ ]* //' \
+        -e 's/\/home\/xiaolonghuang\/l4t-gcc\/bin\/aarch64-buildroot-linux-gnu-g++/\/usr\/bin\/g++/'
 
 echo Sync back build artifacts
-rsync -a --info=progress2 --progress g:/home/xiaolonghuang/mllm/build/  ./build/
+rsync -a ${PROGRESS_ARGS}  g:/home/xiaolonghuang/mllm/build/  ./build/ 
 
 rm ./compile_commands.json
 sed -i 's/\/home\/xiaolonghuang\/mllm/\/home\/xiaolong\/mllm/g' ./build/compile_commands.json
