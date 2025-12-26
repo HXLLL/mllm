3fca7f20847155803995e9a44788dd8440917f4e
Xiaolong Huang <xiaoloonghuang@gmail.com>
2025-12-26 17:07:56 +0800

    edit compile_commands after remote build

--- scripts/remote_build.sh
+++ scripts/remote_build.sh
@@ -2,3 +2,15 @@
 
 echo Synchronizing source code
 rsync -a --info=progress2 --progress ./ g:/home/xiaolonghuang/mllm/ --exclude={"data","perf.*",".git","build"}
+
+ssh g 'source ~/.zshrc; cd /home/xiaolonghuang/mllm/ && python3 task.py tasks/build_jetson_cross.yaml'
+
+echo Sync back build artifacts
+rsync -a --info=progress2 --progress g:/home/xiaolonghuang/mllm/build/  ./build/
+
+rm ./compile_commands.json
+sed -i 's/\/home\/xiaolonghuang\/mllm/\/home\/xiaolong\/mllm/g' ./build/compile_commands.json
+sed -i 's/--sysroot[^ ]* //' ./build/compile_commands.json
+sed -i 's/\/home\/xiaolonghuang\/l4t-gcc\/bin\/aarch64-buildroot-linux-gnu-g++/\/usr\/bin\/g++/' ./build/compile_commands.json
+
+ln -sf ./build/compile_commands.json ./compile_commands.json
