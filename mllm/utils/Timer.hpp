489896f73ec5de19e7d2be6a0bccfed383ae9cc1
Xiaolong Huang <xiaoloonghuang@gmail.com>
2026-01-05 19:26:53 +0800

    timer utility

---
+++ mllm/utils/Timer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <chrono>
+#include <optional>
+
+namespace mllm {
+
+class Timer {
+  using clk = std::chrono::high_resolution_clock;
+  clk::time_point start_;
+  std::optional<clk::time_point> stop_;
+
+public:
+  Timer() : start_(clk::now()) {}
+  void start() { start_ = clk::now(); stop_.reset(); }
+  void stop() { stop_ = clk::now(); }
+  [[nodiscard]] auto elapsed() const {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(stop_.value_or(clk::now()) - start_);
+  }
+};
+
+}  // namespace mllm
