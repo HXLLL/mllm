19ce93eee0782a7246dec0d3ce73641c6a744859
Xiaolong Huang <xiaoloonghuang@gmail.com>
2026-01-15 17:35:41 +0800

    refactor grid scheduler

--- mllm/models/qwen3_i/grid_scheduler.cpp
+++ mllm/models/qwen3_i/grid_scheduler.cpp
@@ -9,19 +9,45 @@ namespace mllm::models::qwen3_i {
 GridScheduler::GridScheduler(int num_layers, int num_chunks, GenerationState& state, ParameterLoader& parameter_loader)
     : state_(state),
       parameter_loader_(parameter_loader),
-      layer_param_tasks_(num_layers),
       num_layers_(num_layers),
       num_chunks_(num_chunks),
+      layer_param_tasks_(num_layers),
       grid_(num_layers * num_chunks) {}
 
-void GridScheduler::setLayerParamTask(int layer, std::unique_ptr<GridTask> load_param) {
-  layer_param_tasks_[layer] = std::move(load_param);
+void GridScheduler::initLayerParamTask(int layer, LoadParamTask load_param) {
+  layer_param_tasks_[layer].emplace(std::move(load_param));
+}
+
+void GridScheduler::initGridCell(int layer, int chunk, GridCell cell) {
+  grid_[layer * num_chunks_ + chunk].emplace(std::move(cell));
+}
+
+bool GridScheduler::isInitDone() const {
+  // Check all layer param tasks are initialized
+  for (int layer = 0; layer < num_layers_; ++layer) {
+    if (!layer_param_tasks_[layer].has_value()) {
+      return false;
+    }
+  }
+
+  // Check all grid cells are initialized
+  for (int layer = 0; layer < num_layers_; ++layer) {
+    for (int chunk = 0; chunk < num_chunks_; ++chunk) {
+      if (!grid_[layer * num_chunks_ + chunk].has_value()) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 void GridScheduler::run() {
-  while (true) {
+  MLLM_RT_ASSERT(isInitDone());
+
+  while (!isDone()) {
     GridTask* task = selectNext();
-    if (!task) { break; }
+    if (!task) { continue; }
     task->execute();
   }
 }
--- mllm/models/qwen3_i/grid_scheduler.hpp
+++ mllm/models/qwen3_i/grid_scheduler.hpp
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <memory>
+#include <optional>
 #include <vector>
 
 #include "mllm/models/qwen3_i/grid_task.hpp"
@@ -13,25 +13,32 @@ namespace mllm::models::qwen3_i {
 class GridScheduler {
  public:
   GridScheduler(int num_layers, int num_chunks, GenerationState& state, ParameterLoader& parameter_loader);
+  virtual ~GridScheduler() = default;
 
-  void setLayerParamTask(int layer, std::unique_ptr<GridTask> load_param);
-  void initGridCell(int layer, int chunk, GridCell cell) { grid_[layer * num_chunks_ + chunk].emplace(std::move(cell)); }
+  void initLayerParamTask(int layer, LoadParamTask load_param);
+  void initGridCell(int layer, int chunk, GridCell cell);
 
   void run();
 
+ protected:
+  virtual GridTask* selectNext() = 0;
+  [[nodiscard]] virtual bool isDone() const = 0;
+
+  [[nodiscard]] bool isInitDone() const;
+  [[nodiscard]] bool isComputeReady(int layer, int chunk) const;
+
   [[nodiscard]] GridCell& getCell(int layer, int chunk) { return *grid_[layer * num_chunks_ + chunk]; }
   [[nodiscard]] const GridCell& getCell(int layer, int chunk) const { return *grid_[layer * num_chunks_ + chunk]; }
+  [[nodiscard]] LoadParamTask& getLayerParamTask(int layer) { return *layer_param_tasks_[layer]; }
+  [[nodiscard]] const LoadParamTask& getLayerParamTask(int layer) const { return *layer_param_tasks_[layer]; }
 
- protected:
-  virtual GridTask* selectNext();
-  [[nodiscard]] bool isComputeReady(int layer, int chunk) const;
   GenerationState& state_;
   ParameterLoader& parameter_loader_;
-  std::vector<std::unique_ptr<GridTask>> layer_param_tasks_;
   const int num_layers_;
   const int num_chunks_;
 
  private:
+  std::vector<std::optional<LoadParamTask>> layer_param_tasks_;
   std::vector<std::optional<GridCell>> grid_;
 };
 
