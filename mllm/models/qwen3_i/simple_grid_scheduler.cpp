ff0438dbbd904a4c24548d9062dbb46167b9d240
Xiaolong Huang <xiaoloonghuang@gmail.com>
2026-01-15 17:35:24 +0800

    simple grid scheduler implementation

---
+++ mllm/models/qwen3_i/simple_grid_scheduler.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) MLLM Team.
+// Licensed under the MIT License.
+
+#include "mllm/models/qwen3_i/simple_grid_scheduler.hpp"
+
+namespace mllm::models::qwen3_i {
+
+GridTask* SimpleGridScheduler::selectNext() {
+  // Check layer param tasks first
+  for (int layer = 0; layer < num_layers_; ++layer) {
+    auto& task = getLayerParamTask(layer);
+    if (!task.isCompleted()) {
+      return &task;
+    }
+  }
+
+  // Check all grid cells for LoadKV, LoadH, and Compute tasks
+  for (int layer = 0; layer < num_layers_; ++layer) {
+    for (int chunk = 0; chunk < num_chunks_; ++chunk) {
+      auto& cell = getCell(layer, chunk);
+
+      if (!cell.load_kv.isCompleted()) {
+        return &cell.load_kv;
+      }
+
+      // Check LoadH
+      if (!cell.load_h.isCompleted()) {
+        return &cell.load_h;
+      }
+
+      // Check Compute (must also be ready)
+      if (!cell.compute.isCompleted() && isComputeReady(layer, chunk)) {
+        return &cell.compute;
+      }
+    }
+  }
+
+  done_ = true;
+
+  return nullptr;
+}
+
+bool SimpleGridScheduler::isDone() const {
+  return done_;
+}
+
+}  // namespace mllm::models::qwen3_i
---
+++ mllm/models/qwen3_i/simple_grid_scheduler.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) MLLM Team.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "mllm/models/qwen3_i/grid_scheduler.hpp"
+
+namespace mllm::models::qwen3_i {
+
+class SimpleGridScheduler : public GridScheduler {
+ public:
+  SimpleGridScheduler(int num_layers, int num_chunks, GenerationState& state, ParameterLoader& parameter_loader)
+      : GridScheduler(num_layers, num_chunks, state, parameter_loader) {}
+
+ protected:
+  GridTask* selectNext() override;
+  [[nodiscard]] bool isDone() const override;
+
+ private:
+  bool done_;
+};
+
+}  // namespace mllm::models::qwen3_i
